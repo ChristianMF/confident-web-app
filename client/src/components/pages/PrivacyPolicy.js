a2ab982aa1467e681fb228745ac9cc00366423f2
Chis <mercadoflores@hotmail.com>
2021-01-24 12:43:51 -0600

    fourth partial version

--- client/src/components/pages/PrivacyPolicy.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import PrivacyPolicyDesign from './PrivacyPolicyDesign'
-
-const PrivacyPolicy = () => (
-        <PrivacyPolicyDesign />
-    )
-
-export default PrivacyPolicy;
