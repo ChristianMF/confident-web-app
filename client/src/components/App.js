a087f590b159c713f22feb7850679f895ba9fb1e
Chis <mercadoflores@hotmail.com>
2021-01-24 12:38:13 -0600

    second partial version

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,11 +1,7 @@
 import React from 'react';
 import { BrowserRouter, Route } from 'react-router-dom';
 
-import Header from './Header';
 import Landing from './pages/Landing';
-import Confident from './pages/Confident';
-import Contacts from './pages/Contacts';
-import PrivacyPolicy from './pages/PrivacyPolicy';
 
 const App = () => (
         <div>
@@ -13,9 +9,6 @@ const App = () => (
                 <div className="container" style={{width: '100%'}}>
                     
                     <Route exact path="/" component={Landing} />
-                    <Route exact path="/confident" component={Confident}/>
-                    <Route exact path="/contacts" component={Contacts} />
-                    <Route exact path="/privacyPolicy" component={PrivacyPolicy} />
                 </div>
             </BrowserRouter>
         </div>
--- client/src/components/pages/Landing.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import LandingDesign from './LandingDesign'
-
-const Landing = () => (
-    <LandingDesign />
-    )
-
-export default Landing;
